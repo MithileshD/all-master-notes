e553be7148d7ddb60012adc5a2ccd29e5afb46e4
Deshpande <861990@cognizant.com>
2025-02-28 20:05:03 +0530

    updated read me

---
+++ README-template.md
@@ -0,0 +1,47 @@
+[[_TOC_]]
+
+## Table of Contents
+ 1. [Subheading 1](#subheading-1)
+ 2. [Subheading 2](#subheading-2)
+ 3. [Subheading 3](#sub-heading-3)
+
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Section Heading - Available Scripts
+
+In the project directory, you can run:
+
+### Sub-Section Heading `npm start`
+
+### Code Splitting
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
+
+### Analyzing the Bundle Size
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
+
+### Making a Progressive Web App
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
+
+### Advanced Configuration
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
+
+### Deployment
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
+
+### `npm run build` fails to minify
+
+This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+
+ ## Subheading 1
+ Content of the subheading 1
+ ## Subheading 2
+ Content of the subheading 2
+ ## Sub heading 3
+ Content of the subheading 3
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,104 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ParentUseEffExe from './quizes/1111/ParentHook'
-
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ParentUseEffExe from "./quizes/1111/ParentHook";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [isDisabled, setIsDisbaled] = useState(false);
+  const [toDoList, setToDoList] = useState([]);
 
   // console.log('step 1')
   // setTimeout(()=>console.log('step 2'),0)
   // Promise.resolve('Step3').then((res)=>console.log(res + 'step 3'))
   // console.log('step 4')
 
+  const handleChange = (e) => {
+    switch (e.target.id) {
+      case "name-key":
+        setName(e.target.value);
+        break;
+
+      case "email-key":
+        setEmail(e.target.value);
+        break;
+
+      default:
+        break;
+    }
+    // return;
+  };
+
+  const fetchData = async () => {
+    return await axios.get("https://dummyjson.com/todos?limit=10&skip=0");
+  };
+
+  const handleSubmit = (e) => {
+    if (name && name !== "" && email && email !== "") setIsDisbaled(true);
+    const response = fetchData();
+
+    response.then((res) => {
+      console.log(res.data.todos);
+
+      setToDoList(res.data.todos)
+    });
+
+    // switch (e.target.id) {
+    //   case "name-key":
+    //     setName(e.target.value);
+    //     break;
+
+    //   case "email-key":
+    //     setEmail(e.target.value);
+    //     break;
+
+    //   default:
+    //     break;
+    // }
+    // return;
+  };
+
+  // useEffect(() => {
+  //   if (name && name !== "" && email && email !== "") setIsDisbaled(true);
+  // }, []);
+
+  console.log('todo', toDoList);
+  
   return (
     <>
-    <ParentUseEffExe/>
+      {/* <ParentUseEffExe /> */}
+      {/* <div> */}
+
+      <div>Name</div>
+      <input id="name-key" value={name} onChange={(e) => handleChange(e)} />
+
+      <div>Email</div>
+      <input id="email-key" value={email} onChange={(e) => handleChange(e)} />
+
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <button disabled={isDisabled} onClick={(e) => handleSubmit(e)}>
+          Submit{" "}
+        </button>
+
+        {/* <ul>
+          <li>
+            1st Todo
+          </li>
+        </ul> */}
+
+        <ul>
+          {toDoList.map((toDo) => {
+            console.log('toDo', toDo);
+            return <li>{toDo.todo}</li>;
+          })}
+        </ul>
+      </div>
+
+      {/* <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
@@ -32,12 +113,12 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
-      <p className="read-the-docs">
+      </div> */}
+      {/* <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
